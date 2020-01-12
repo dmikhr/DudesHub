a58226ecd87ba9073d87446abe075cfca2ce7275
dmikhr <dmitrypkh@gmail.com>
2020-01-12 14:52:00 +0300

    experimenting with Github API

---
+++ app/services/github_service.rb
@@ -0,0 +1,68 @@
+# docs: https://octokit.github.io/octokit.rb/Octokit/Client/Gists.html
+# https://octokit.github.io/octokit.rb/Octokit/Client/PullRequests.html
+# gs = GithubService.new
+# gs.call
+
+class GithubService
+  attr_reader: :repos
+
+  def initialize
+    @client = octokit_client
+  end
+
+  def call
+    @result = @client.repositories
+  end
+
+  def get_user_repos
+    @repos = @client.repositories
+  end
+
+  def repos_list
+    @repos.map { |repo| [repo[:name], repo[:full_name], repo[:id]] }
+  end
+
+  def find_repo_by_name(name)
+    # potentially useful keys: :full_name, :id, :private, [:owner][:login], :description, :html_url
+    repo = @repos.select { |repo| repo[:name] == name }
+    repo.first unless repo.empty?
+  end
+
+  # get a list of open pull request for a given repo
+  # https://octokit.github.io/octokit.rb/Octokit/Client/PullRequests.html#pull_requests-instance_method
+  def get_pull_requests(repo_full_name)
+    @client.pull_requests(repo_full_name, state: 'open')
+  end
+
+  # https://octokit.github.io/octokit.rb/Octokit/Client/PullRequests.html#pull_request-instance_method
+  # Octokit.pull_request('rails/rails', 42, :state => 'closed')
+  # :url, :id, :state, :number, :title
+  def get_pull_request(repo_name, id)
+    @client.pull_request(repo_name, id)
+  end
+
+  # https://developer.github.com/v3/guides/working-with-comments/
+  # https://developer.github.com/v3/issues/comments/#create-a-comment
+  # https://octokit.github.io/octokit.rb/Octokit/Client/Issues.html#add_comment-instance_method
+  def create_pull_request_comment(repo_full_name, comment)
+    # comment = 'https://camo.githubusercontent.com/097ad4338d515251c51ef24d9416e554c469b8b9/687474703a2f2f6b6872616d74736f762e6e65742f66696c65732f64756465735f646966662e7376673f73616e6974697a653d74727565'
+
+    # Issue comments in pull request https://stackoverflow.com/questions/16744069/create-comment-on-pull-request
+    repo = find_repo_by_name(repo_full_name)
+    comments_url = repo[:_links][:comments]
+    # sample comments_url: https://api.github.com/repos/dmikhr/test_repo_dude/issues/1/comments
+    pull_request_issue_id = comments_url.split('/')[-2]
+
+    @client.add_comment(repo_full_name, pull_request_issue_id, comment)
+  end
+
+  private
+
+  def octokit_client
+    client = Octokit::Client.new(access_token: access_token)
+  end
+
+  def access_token
+    Rails.application.credentials[Rails.env.to_sym][:github][:access_token]
+  end
+end
