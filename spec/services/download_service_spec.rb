b03466b5e3588883b3c1cf9fc3b14ccbda6b7f72
dmikhr <dmitrypkh@gmail.com>
2020-01-17 16:42:20 +0300

    download file by line feature

--- spec/services/download_service_spec.rb
+++ spec/services/download_service_spec.rb
@@ -6,8 +6,14 @@ RSpec.describe DownloadService, type: :service do
   describe 'DownloadService' do
     vcr_options = { :record => :new_episodes }
 
-    it '.call', vcr: vcr_options do
+    it '.call default', vcr: vcr_options do
       expect(DownloadService.call(url).class).to be String
+    end
+
+    it '.call read_by_line', vcr: vcr_options do
+      expect(DownloadService.call(url, :read_by_line).class).to be Array
+      expect(DownloadService.call(url, :read_by_line)).to_not be_empty
+      expect(DownloadService.call(url, :read_by_line).first.class).to be String
     end
   end
 end
