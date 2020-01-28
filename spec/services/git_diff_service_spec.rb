1e2201669b5fc622d7457a8b5bfd87b4ed30671f
dmikhr <dmitrypkh@gmail.com>
2020-01-28 11:23:27 +0300

    change Git diff service methods from instance to class

--- spec/services/git_diff_service_spec.rb
+++ spec/services/git_diff_service_spec.rb
@@ -2,17 +2,16 @@ require 'rails_helper'
 
 RSpec.describe GitDiffService, type: :service do
   let!(:diff) { file_fixture('diff_sample.txt').readlines }
-  let(:git_diff_service) { GitDiffService.new(diff) }
 
   describe 'GitDiffService' do
     vcr_options = { :record => :new_episodes }
 
     it '#call', vcr: vcr_options do
-      expect(git_diff_service.call.class).to be Array
-      expect(git_diff_service.call).to_not be_empty
+      expect(GitDiffService.call(diff).class).to be Array
+      expect(GitDiffService.call(diff)).to_not be_empty
 
-      expect(git_diff_service.call.first.class).to be Hash
-      expect(git_diff_service.call.first.keys.sort).to be == %i[old_name new_name status].sort
+      expect(GitDiffService.call(diff).first.class).to be Hash
+      expect(GitDiffService.call(diff).first.keys.sort).to be == %i[old_name new_name status].sort
     end
   end
 end
