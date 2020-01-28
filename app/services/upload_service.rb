357bece96749e18b95b31be8c176552afd881720
dmikhr <dmitrypkh@gmail.com>
2020-01-28 11:07:07 +0300

    change Upload service methods from instance to class

--- app/services/upload_service.rb
+++ app/services/upload_service.rb
@@ -1,42 +1,38 @@
 require 'aws-sdk-s3'
 
 class UploadService
-  BUCKET_FOLDER = 'dudeshub'
-  AWS_FNAME = 'dudesgl_file.svg'
+  class << self
+    BUCKET_FOLDER = 'dudeshub'
+    IMAGE_EXT = '.svg'
 
-  def initialize(data = '')
-    # @data = data
-    # emulate input from DudeGL (svg string)
-    @data = File.open(Rails.application.credentials[:test_file_path], "rb").read
-  end
+    # https://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpRuby.html
+    # use second method from amazon docs if DudeGL will pass image as String
+    def call(data, aws_fname)
+      client = Aws::S3::Client.new(region: credentials(:region),
+                                   secret_access_key: credentials(:secret_access_key),
+                                   access_key_id: credentials(:access_key_id))
 
-  # https://docs.aws.amazon.com/AmazonS3/latest/dev/UploadObjSingleOpRuby.html
-  # use second method from amazon docs if DudeGL will pass image as String
-  def call
-    client = Aws::S3::Client.new(region: credentials(:region),
-                                 secret_access_key: credentials(:secret_access_key),
-                                 access_key_id: credentials(:access_key_id))
+      @object_key = "#{BUCKET_FOLDER}/#{aws_fname}#{IMAGE_EXT}"
+      s3 = Aws::S3::Resource.new(client: client)
+      obj = s3.bucket(credentials(:aws_bucket)).object(@object_key)
+      # upload string in I/O stream, avoiding need to save it locally as tmp file
+      obj.put(body: data)
+      # make file public
+      # https://docs.aws.amazon.com/sdkforruby/api/Aws/S3/Client.html#put_object_acl-instance_method
+      client.put_object_acl(bucket: credentials(:aws_bucket),
+                            grant_read: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
+                            key: @object_key)
+      s3_file_path
+    end
 
-    @object_key = "#{BUCKET_FOLDER}/#{AWS_FNAME}"
-    s3 = Aws::S3::Resource.new(client: client)
-    obj = s3.bucket(credentials(:aws_bucket)).object(@object_key)
-    # upload string in I/O stream, avoiding need to save it locally as tmp file
-    obj.put(body: @data)
-    # make file public
-    # https://docs.aws.amazon.com/sdkforruby/api/Aws/S3/Client.html#put_object_acl-instance_method
-    client.put_object_acl(bucket: credentials(:aws_bucket),
-                          grant_read: "uri=http://acs.amazonaws.com/groups/global/AllUsers",
-                          key: @object_key)
-    s3_file_path
-  end
+    private
 
-  private
+    def credentials(key)
+      Rails.application.credentials[:aws][key]
+    end
 
-  def credentials(key)
-    Rails.application.credentials[:aws][key]
-  end
-
-  def s3_file_path
-    "https://#{credentials(:aws_bucket)}.s3-#{credentials(:region)}.amazonaws.com/#{@object_key}"
+    def s3_file_path
+      "https://#{credentials(:aws_bucket)}.s3-#{credentials(:region)}.amazonaws.com/#{@object_key}"
+    end
   end
 end
--- spec/services/upload_service_spec.rb
+++ spec/services/upload_service_spec.rb
@@ -2,10 +2,9 @@ require 'rails_helper'
 
 RSpec.describe UploadService, type: :service do
   let(:data_string) { File.open(Rails.application.credentials[:test_file_path], "rb").read }
-  let(:upload_service) { UploadService.new(data_string) }
 
   it 'call UploadService' do
-    expect(upload_service).to receive(:call)
-    upload_service.call
+    expect(UploadService).to receive(:call)
+    UploadService.call(data_string, 'test.svg')
   end
 end
