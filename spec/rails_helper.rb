fea930ab592e454ed0f4bc0eafb77f19feafe656
dmikhr <dmitrypkh@gmail.com>
2020-02-09 11:37:55 +0300

    spec helpers update

--- spec/rails_helper.rb
+++ spec/rails_helper.rb
@@ -36,6 +36,8 @@ RSpec.configure do |config|
   config.include FactoryBot::Syntax::Methods
   config.include OmniauthMacros
   config.include Devise::Test::ControllerHelpers, type: :controller
+  config.include FeatureHelpers, type: :feature
+  config.include ControllerHelpers, :type => :controller
   # Remove this line if you're not using ActiveRecord or ActiveRecord fixtures
   config.fixture_path = "#{::Rails.root}/spec/fixtures"
 
@@ -64,16 +66,17 @@ RSpec.configure do |config|
   # arbitrary gems may also be filtered via:
   # config.filter_gems_from_backtrace("gem name")
 
-Shoulda::Matchers.configure do |config|
-  config.integrate do |with|
-    with.test_framework :rspec
-    with.library :rails
+  Shoulda::Matchers.configure do |config|
+    config.integrate do |with|
+      with.test_framework :rspec
+      with.library :rails
+    end
   end
-end
 
-FactoryBot::SyntaxRunner.class_eval do
-  include ActionDispatch::TestProcess
+  FactoryBot::SyntaxRunner.class_eval do
+    include ActionDispatch::TestProcess
+  end
+
 end
 
 OmniAuth.config.test_mode = true
-end
